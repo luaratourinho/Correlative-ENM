69d757e64bbe96919bdc29f3f5b520f256d6d71b
Luara Tourinho <luatourinho@gmail.com>
2021-05-15 10:26:01 -0300

    cropping the selected variables, saving 1 by 1

--- R/5_crop_future_scenarios.R
+++ R/5_crop_future_scenarios.R
@@ -29,7 +29,7 @@ library(rgdal)
 
 ## Future variables ------------------------------------------------------
 
-to_crop <- raster('./data/env/present/wc2.1_2.5m_bio_1.tif')
+to_crop <- raster('./data/env_cropped/present/_wc2.1_2.5m_bio_1.tif')
 #to_crop <- envi.mask2[[1]]
 
 # If future biovariables come separately, build a list like you did before
@@ -39,151 +39,40 @@ to_crop <- raster('./data/env/present/wc2.1_2.5m_bio_1.tif')
 
 # spp126 ------------------------------------------------------------------
 
-# Ps. 2021-2040 was done in the step 5_crop_environmental_variables.R
+# Ps. 2021-2040 was done in the step "5_crop_environmental_variables.R"
+
+# Choosing another future scenario
 
 # 2041-2060
 
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp126_2041-2060.tif")
+future_var <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp126_2041-2060.tif")
+
+# Chose the variables that you selected in "5_crop_environmental_variables.R"
+future_var_stk <- future_var[[c(3,8,15,18)]]
 envi_fut.cut<- crop(future_var_stk, to_crop)
-
-# Save all variables
 envi_fut.mask<- mask(envi_fut.cut, to_crop)
-dir.create(paste0("./data/env_cropped/future/bc25_ssp126_41-60"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp126_41-60/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
 
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
+# Save only that selected variables 5_crop_environmental_variables.R step
+names(envi_fut.mask) <- c("bio_3", "bio_8", "bio_15","bio_18")
 dir.create(paste0("./data/env_sel/future/bc25_ssp126_41-60"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp126_41-60/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
+writeRaster(envi_fut.mask, filename='./data/env_sel/future/bc25_ssp126_41-60/', 
+            format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
 
 
-# 2061-2080
+# However, that write function above is adding a "_" before the raster names
+# because that I am saving one by one
 
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp126_2061-2080.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp126_61-80"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp126_61-80/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp126_61-80"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp126_61-80/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
+# Saving one by one
+writeRaster(envi_fut.mask[[1]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_3', 
+            format="GTiff", overwrite=TRUE)
+writeRaster(envi_fut.mask[[2]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_8', 
+            format="GTiff", overwrite=TRUE)
+writeRaster(envi_fut.mask[[3]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_15', 
+            format="GTiff", overwrite=TRUE)
+writeRaster(envi_fut.mask[[4]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_18', 
+            format="GTiff", overwrite=TRUE)
 
 
 
-# 2081-2100
-
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp126_2081-2100.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp126_81-00"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp126_81-00/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp126_81-00"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp126_81-00/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-
-# BCC spp585 --------------------------------------------------------------
-
-
-# 2021-2040
-
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp585_2021-2040.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp585_21-40"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp585_21-40/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp585_21-40"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp585_21-40/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# 2041-2060
-
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp585_2041-2060.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp585_41-60"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp585_41-60/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp585_41-60"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp585_41-60/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# 2061-2080
-
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp585_2061-2080.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp585_61-80"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp585_61-80/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp585_61-80"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp585_61-80/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-
-# 2081-2100
-
-future_var_stk <- stack("./data/env/future/wc2.1_2.5m_bioc_BCC-CSM2-MR_ssp585_2081-2100.tif")
-envi_fut.cut<- crop(future_var_stk, to_crop)
-envi_fut.mask<- mask(envi_fut.cut, to_crop)
-
-# Save all variables
-dir.create(paste0("./data/env_cropped/future/bc25_ssp585_81-00"))
-writeRaster(envi_fut.mask, filename='./data/env_cropped/future/bc25_ssp585_81-00/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# Save only that selected variables 
-envi_fut_sel <- envi_fut.mask[[c(3,8,15,18)]]
-names(envi_fut_sel) <- c("bio3", "bio8", "bio15","bio18")
-#dir.create(paste0("./data/env_sel/future/bc25_ssp585_81-00"))
-writeRaster(envi_fut_sel, filename='./data/env_sel/future/bc25_ssp585_81-00/', format="GTiff", 
-            bylayer=TRUE, suffix="names", overwrite=TRUE)
-
+# Repeat this routine for other scenarios, GCMs, and years that interest you
 
